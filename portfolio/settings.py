b8a45c02a01215fc0c61f803cd32f5986ad012de
Shubham Rathore <shubhamrathore291@gmail.com>
2021-08-20 11:57:01 +0530

    resolve req. image issue in update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
-# ie if Heroku server
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+# # ie if Heroku server
+# if 'DATABASE_URL' in os.environ:
+#     import dj_database_url
+#     DATABASES = {'default': dj_database_url.config()}
